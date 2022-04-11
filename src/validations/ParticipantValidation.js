3ea8311bb78dc17bf475942fa47915d52529da4d
taufiq <ahmadtaufiqhdyt@gmail.com>
2022-04-11 16:31:27 +0700

    add validation

--- src/validations/ParticipantValidation.js
+++ src/validations/ParticipantValidation.js
@@ -5,6 +5,11 @@ const ParticipantValidation = (values) => {
       errors.full_name = "Name must be filled!";
     }
   
+    let fn = String(values.full_name)
+    if (fn.length > 20) {
+      errors.full_name = "Max 20 characters";
+    }
+
     if (!values.business_name || values.business_name === "") {
       errors.business_name = "Business name must be filled";
     }
@@ -13,8 +18,18 @@ const ParticipantValidation = (values) => {
       errors.email = "Email must be filled";
     }
 
+    let email = String(values.email).toLocaleLowerCase()
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    if (!regex.test(email)){
+      errors.phone_number = "Email format does not match";
+    }
+
     if (!values.phone_number || values.phone_number === "") {
       errors.phone_number = "Phone number must be filled";
+    }
+    let pn = String(values.phone_number)
+    if (pn.length < 8 || pn.length > 13) {
+      errors.phone_number = "The format of the cellphone number is at least 8, maximum is 13";
     }
     return errors
   };
